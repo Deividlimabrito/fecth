23de199a8b4db87a14cd2a12bb2c042da5cc8af7
deivid <deividlima1320@gmai.com>
2022-10-01 17:12:59 -0300

    aula de fetch

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-let generica;
-fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa')
- .then(function(response){
-    return response.json();
- })
-.then(function (body){
-    console.log("body",body);
-    generica = body;
-});
-console.log('generica',generica);
+function retornoPromessaFetch(resposta) {
+    let promessaBody = resposta.json();
+    return promessaBody;
+}
+function retornoPromessaBody(body) {
+    lista.innerHTML = "";
+    body.forEach(function (pessoa) {
+        //console.log(pessoa);
+        lista.innerHTML = lista.innerHTML + "<li>" + pessoa.nome + "</li>";
+    });
+}
+function atualizar() {
+    let promessa = fetch("https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa")
+    promessa.then(retornoPromessaFetch).then(retornoPromessaBody);
+} function adicionar() {
+    let nomeDaPessoa = nome.value;
+    console.log(nomeDaPessoa);
+    let requestPost = {
+        method: "Post",
+        h'eaders: {
+            "content-type": "application/json"
+        },
+        //conteudo que eu desejo enviar
+        body: JSON.stringify({
+            idade: 20, nome: nomeDaPessoa,
+        })
+    };
+    fetch("https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa", requestPost).then(function (resposta) {
+        console.log(resposta);
+    })
+
+}
